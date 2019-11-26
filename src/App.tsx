8ab7d1d4d37632eeeb7c72e64d6ac45f1d133295
maximedaniel <maxaxeldaniel@gmail.com>
2019-11-26 16:55:17 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,9 @@ function App() {
     <Grid  style={{padding:'1em'}}>
       <Grid.Row>
         <Grid.Column width={4}>
-        <Segment>
-          <Header as='h3'>Sommaire</Header>
-          <List animated verticalAlign='middle' size='large'>
-            <List.Item as='a'>
-              <Icon name='users' />
-              <List.Content>
-                <List.Header>Helen</List.Header>
-              </List.Content>
-            </List.Item>
-            <List.Item as='a'>
-            <Icon  name='users' />
-              <List.Content>
-                <List.Header>Christian</List.Header>
-              </List.Content>
-            </List.Item>
-            <List.Item as='a'>
-            <Icon  name='users' />
-              <List.Content>
-                <List.Header>Daniel</List.Header>
-              </List.Content>
-            </List.Item>
-          </List>
-        </Segment>
+          <Profil />
         </Grid.Column>
         <Grid.Column width={12}>
-          <Profil />
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/profil.tsx
+++ src/profil.tsx
@@ -1,46 +1,69 @@
 import React, { Component }  from 'react';
-import { Image, Grid, List } from 'semantic-ui-react'
+import { Image, Grid, List, Icon, Label, Header, GridRow, Segment } from 'semantic-ui-react'
 
-class Profil extends Component {
+interface Props{
+
+}
+interface State{
+  hover: boolean
+}
+
+class Profil extends Component<Props, State> {
   constructor(props: any){
     super(props);
+    this.state = {
+      hover:false
+    }
   }
 
-  render() {
+  handleOnMouseOver = (event: any) => {
+    this.setState({hover: !this.state.hover});
+  }
+
+  render = ()  => {
     return (
+      <Segment raised={this.state.hover} onMouseEnter={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseOver}>
         <Grid>
         <Grid.Row centered>
-            <Grid.Column width={4}>
-            <Image src='/img/profil0.jpg' size='medium' circular />
+            <Grid.Column textAlign='center' width={16}>
+              <Image circular src='/img/profil0.jpg' size='small' centered/>
+            <Header as='h3'>
+                <Header.Content>
+                  Maxime DANIEL
+                  <Header.Subheader>PhD in Computer Science</Header.Subheader>
+                </Header.Content>
+            </Header>
             </Grid.Column>
         </Grid.Row>
         <Grid.Row centered>
-            <Grid.Column width={4}>
-            <List textAlign='center'>
+            <Grid.Column textAlign='center' width={16}>
+            <List>
                 <List.Item>
-                <List.Icon name='users' />
-                <List.Content>Semantic UI</List.Content>
+                  <List.Icon name='inbox'   flipped='horizontally'/>
+                  <List.Content>maxaxeldaniel[at]gmail.com</List.Content>
                 </List.Item>
                 <List.Item>
-                <List.Icon name='marker' />
-                <List.Content>New York, NY</List.Content>
+                  <List.Icon name='phone' flipped='horizontally' />
+                  <List.Content>+33 (0)6 98 41 24 84</List.Content>
                 </List.Item>
                 <List.Item>
-                <List.Icon name='mail' />
-                <List.Content>
-                    <a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>
-                </List.Content>
+                  <List.Icon name='map marker alternate'  flipped='horizontally' />
+                  <List.Content>Biarritz, France</List.Content>
                 </List.Item>
-                <List.Item>
-                <List.Icon name='linkify' />
-                <List.Content>
-                    <a href='http://www.semantic-ui.com'>semantic-ui.com</a>
-                </List.Content>
-                </List.Item>
-            </List>
+              </List>
+              
+              <Label as='a' onClick= {() => window.open("https://github.com/maximedaniel", "_blank")}>
+                <Icon name='github' />
+                Github
+              </Label>
+              <Label as='a' onClick= {() => window.open("https://www.youtube.com/channel/UCX7uHVWqJ6TxoQnIXhVZ2gw", "_blank")}>
+              <Icon name='youtube' />
+              Youtube
+            </Label>
             </Grid.Column>
         </Grid.Row>
         </Grid>
+      </Segment>
     );
   }
 }

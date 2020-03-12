9c45157a48f2feca96049dca5c89b7d5d0b66b84
maximedaniel <maxaxeldaniel@gmail.com>
2020-03-12 18:16:07 +0100

    - Adding .pdf of publications in directory public/pdf - Adding url link to public/pdf for each publication - Adding bold to "Computer Vision" in Methodology

--- src/components/methodology.tsx
+++ src/components/methodology.tsx
@@ -41,7 +41,7 @@ class Methodology extends Component<Props, State> {
                     <Header as='h5'>
                         2. Designing
                         <Header.Subheader>
-                            <b>Ideating</b> (Brainstorming, 9-Windows, Biomimicry), <b>Sketching</b> (Autodesk Sketchbook), <b>Modeling</b> (Autodesk Fusion 360, OnShape) and <b>Simulating</b> (Unity3D)
+                            <b>Ideating</b> (Brainstorming, 9-Windows, Biomimicry), <b>Sketching</b> (Autodesk Sketchbook), <b>Modeling</b> (Autodesk Fusion 360, OnShape), and <b>Simulating</b> (Unity3D)
                        </Header.Subheader>
                     </Header>
                     </Grid.Column>
@@ -51,7 +51,7 @@ class Methodology extends Component<Props, State> {
                     <Header as='h5'>
                         3. Prototyping
                         <Header.Subheader>
-                            <b>Manufacturing</b> (3D printing, Laser cutting, Thermoforming), <b>Integrating</b> (Raspberry Pi, Arduino, sensors, actuators), <b>Hardware programming</b> (Python, C), <b>Sofware programming</b> (C++/QT5, Java/JavaFX, Python/PyQT, C/Xamarin), Vision programming (C++/OpenCV) and <b>Web programming</b> (Full Stack Django, Full Stack MERN Javascript) </Header.Subheader>
+                            <b>Manufacturing</b> (3D printing, Laser cutting, Thermoforming), <b>Integrating</b> (Raspberry Pi, Arduino, sensors, actuators), <b>Hardware programming</b> (Python, C), <b>Sofware programming</b> (C++/QT5, Java/JavaFX, Python/PyQT, C/Xamarin), <b>Vision programming</b> (C++/OpenCV), and <b>Web programming</b> (Full Stack Django, Full Stack MERN Javascript) </Header.Subheader>
                     </Header>
                     </Grid.Column>
                     </Grid.Row>
@@ -60,7 +60,7 @@ class Methodology extends Component<Props, State> {
                     <Header as='h5'>
                         4. Experimenting
                         <Header.Subheader>
-                            <b>Data gathering</b> (structured questionnaires, semi-structured interviews, realtime measurements with sensors) and <b>Statistics computing</b> (R, Python, NumPy, Pandas, SciPy, StatsModels)
+                            <b>Data gathering</b> (structured questionnaires, semi-structured interviews, realtime measurements with sensors), and <b>Statistics computing</b> (R, Python, NumPy, Pandas, SciPy, StatsModels)
                         </Header.Subheader>
                     </Header>
                     </Grid.Column>
@@ -70,7 +70,7 @@ class Methodology extends Component<Props, State> {
                     <Header as='h5'>
                         5. Reporting
                         <Header.Subheader>
-                        <b>Open sourcing</b> (GitHub, OnShape), <b>Video editing</b> (Adobe After Effect, Blender), <b>Paper writing</b> (LaTeX, Word, Markdown) and <b>Talking</b> (english operational, french native)
+                        <b>Open sourcing</b> (GitHub, OnShape), <b>Video editing</b> (Adobe After Effect, Blender), <b>Paper writing</b> (LaTeX, Word, Markdown), and <b>Talking</b> (english operational, french native)
                         </Header.Subheader>
                     </Header>
                     </Grid.Column>
--- src/components/publication.tsx
+++ src/components/publication.tsx
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react';
-import { Image, Grid, Header} from 'semantic-ui-react'
+import { Icon, Image, Grid, Header, Menu} from 'semantic-ui-react'
 
 interface Props{
 
@@ -32,6 +32,12 @@ class Publication extends Component<Props, State> {
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                         <b>Maxime Daniel</b>, Guillaume Rivière, and Nadine Couture. 2019. CairnFORM: a Shape-Changing Ring Chart Notifying Renewable Energy Availability in Peripheral Locations. <i>In Proceedings of the 13th International Conference on Tangible, Embedded, and Embodied Interaction (TEI '19)</i>. ACM, New York, NY, USA, 275-286.
+                        <Menu.Item
+                          href="pdf/daniel2019cairnform.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
                     </Grid.Column>
                     </Grid.Row>
                     <Grid.Row >
@@ -39,7 +45,14 @@ class Publication extends Component<Props, State> {
                     <Image centered src='img/tei18.png' size= 'tiny'/>
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
-                        <b>Maxime Daniel</b>, Guillaume Rivière, and Nadine Couture. 2018. Designing an Expandable Illuminated Ring to Build an Actuated Ring Chart. <i>In Proceedings of the 12th International Conference on Tangible, Embedded, and Embodied Interaction (TEI '18)</i>. ACM, New York, NY, USA, 140-147.</Grid.Column>
+                        <b>Maxime Daniel</b>, Guillaume Rivière, and Nadine Couture. 2018. Designing an Expandable Illuminated Ring to Build an Actuated Ring Chart. <i>In Proceedings of the 12th International Conference on Tangible, Embedded, and Embodied Interaction (TEI '18)</i>. ACM, New York, NY, USA, 140-147.
+                        <Menu.Item
+                          href="pdf/daniel2018designing.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
+                    </Grid.Column>
                     </Grid.Row>
                     <Grid.Row >
                     <Grid.Column mobile={6} tablet={3} computer={2} verticalAlign='middle'>
@@ -47,6 +60,12 @@ class Publication extends Component<Props, State> {
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                         <b>Maxime Daniel</b>, Guillaume Rivière, and Nadine Couture. 2017. CAIRNS: an Ambient Tangible Interface for Shifting Energy Demand at Work. <i>In Proceedings of the 29th Conference on l'Interaction Homme-Machine (IHM '17)</i>. ACM, New York, NY, USA, 221-231.
+                        <Menu.Item
+                          href="pdf/daniel2017cairns.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
                     </Grid.Column>
                     </Grid.Row>
                     <Grid.Row >
@@ -55,6 +74,12 @@ class Publication extends Component<Props, State> {
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                         <b>Maxime Daniel</b>, Guillaume Rivière, and Nadine Couture. 2016. Persuasive Interactive Systems in public and collective spaces: what Role for Tangible Interfaces ?. <i>In Actes de la 28ième conférence francophone sur l'Interaction Homme-Machine (IHM '16)</i>. ACM, New York, NY, USA, 221-229.
+                        <Menu.Item
+                          href="pdf/daniel2016persuasive.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
                     </Grid.Column>
                     </Grid.Row>
                     <Grid.Row >
@@ -63,6 +88,12 @@ class Publication extends Component<Props, State> {
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                         <b>Maxime Daniel</b>, Guillaume Rivière, Nadine Couture, and Stéphane Kreckelbergh. 2016. An Analysis of Persuasive Technologies for Energy Demand Side Management. <i>In Actes de la 28ième conférence francophone sur l'Interaction Homme-Machine (IHM '16)</i>. ACM, New York, NY, USA, 197-210.
+                        <Menu.Item
+                          href="pdf/daniel2016analysis.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
                     </Grid.Column>
                     </Grid.Row>
                     <Grid.Row >
@@ -71,6 +102,12 @@ class Publication extends Component<Props, State> {
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                         Jérémy Frey, <b>Maxime Daniel</b>, Julien Castet, Martin Hachet, and Fabien Lotte. 2016. Framework for Electroencephalography-based Evaluation of User Experience. <i>In Proceedings of the 2016 CHI Conference on Human Factors in Computing Systems (CHI '16)</i>. ACM, New York, NY, USA, 2283-2294.
+                        <Menu.Item
+                          href="pdf/frey2016framework.pdf"
+                          link
+                        >
+                          <Icon link name="file pdf outline"/>
+                        </Menu.Item>
                      </Grid.Column>
                     </Grid.Row>
                 </Grid>

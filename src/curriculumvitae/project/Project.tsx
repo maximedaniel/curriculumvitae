7ddeffb090c34c1ede4347805a499f94100cdfbc
maximedaniel <maxaxeldaniel@gmail.com>
2021-01-15 17:49:36 +0100

    Adding TEI'21 WiP Paper and Video

--- src/curriculumvitae/project/Project.tsx
+++ src/curriculumvitae/project/Project.tsx
@@ -34,15 +34,28 @@ class Project extends Component<Props, State> {
                     <Grid.Column width={16} verticalAlign='top'>
                     <Card.Group centered>
                     <Card>
+                      <Embed id='XDaJmj2BntU'  iframe={{allowFullScreen: true}} placeholder='/img/allureAndPurposes.jpg' source='youtube'/>
+                      <Card.Content>
+                        <Card.Header>CairnFORM<sup>2</sup></Card.Header>
+                        <Card.Meta>
+                            <span className='date'>Dec 2018 - Nov 2020</span>
+                        </Card.Meta>
+                        <Card.Description>
+                        <b>Keywords:</b> Programmable Matter; Shape-Changing Interfaces; Use Cases; In-Situ Study.<br></br>
+                        <b>Abstract:</b> We extend the understanding of the potential utility and usability of axisymmetric shape-change. We present (1) 16 new use cases for CairnFORM an (2) a two-month comparative field study with CairnFORM in the workplace. Compared with flat-screen animations, early results show that cylindrical shape-change animations keep a better attractiveness over time.
+                        </Card.Description>
+                        </Card.Content>
+                    </Card>
+                    <Card>
                       <Embed id='wZ_3CtDllbA'  iframe={{allowFullScreen: true}} placeholder='/img/cairnform.jpg' source='youtube'/>
                       <Card.Content>
-                        <Card.Header>CairnFORM</Card.Header>
+                        <Card.Header>CairnFORM<sup>1</sup></Card.Header>
                         <Card.Meta>
                             <span className='date'>Oct 2015 - Nov 2018</span>
                         </Card.Meta>
                         <Card.Description>
                         <b>Keywords:</b> Programmable Matter; Shape-Changing Interfaces; Energy Demand Side-Management; Workplace.<br></br>
-                        <b>Abstract:</b> CairnFORM is a stack of expandable illuminated rings for display that can change of cylindrical shape (e.g., cone, double cone, cylinder). We show that cylindrical shape change can be used (1) for informing users around the display through data physicalization, (2) for unobtrusively notifying users around the display through peripheral interaction, (3) for offering a greater user experience over time than a traditional flat display with no shape change.
+                        <b>Abstract:</b> CairnFORM is a stack of expandable illuminated rings for display that can change of axisymmetric shape (e.g., cone, double cone, cylinder). We show that axisymmetric shape-change can be used (1) for informing users around the display through data physicalization, (2) for unobtrusively notifying users around the display through peripheral interaction, (3) for offering a greater user experience over time than a traditional flat display with no shape change.
                         </Card.Description>
                         </Card.Content>
                     </Card>
--- src/curriculumvitae/publication/Publication.tsx
+++ src/curriculumvitae/publication/Publication.tsx
@@ -28,6 +28,14 @@ class Publication extends Component<Props, State> {
                 <Grid padded>
                     <Grid.Row >
                     <Grid.Column mobile={6} tablet={3} computer={2} verticalAlign='middle'>
+                    <Image centered src='img/tei21.png' size= 'tiny'/>
+                    </Grid.Column>
+                    <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
+                        <b>Maxime Daniel</b> and Guillaume Rivière. 2021. xploring Axisymmetric Shape-Change's Purposes and Allure for Ambient Display: 16 Potential Use Cases and a Two-Month Preliminary Study on Daily Notifications. <i>In Proceedings of the 15th International Conference on Tangible, Embedded, and Embodied Interaction (TEI '21)</i>. ACM, New York, NY, USA, X-X (<b>incoming</b>).
+                    </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row >
+                    <Grid.Column mobile={6} tablet={3} computer={2} verticalAlign='middle'>
                     <Image centered src='img/tei19.png' size= 'tiny'/>
                     </Grid.Column>
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>

c64426b8f50837c1d2700480121c7624e86aff06
Maxime Daniel <maxaxeldaniel@gmail.com>
2025-10-15 17:37:33 +0200

    added review

--- src/curriculumvitae/review/Review.tsx
+++ src/curriculumvitae/review/Review.tsx
@@ -30,7 +30,7 @@ class Review extends Component<Props, State> {
                     <Grid.Column textAlign='left' verticalAlign='middle' mobile={10} tablet={13}  computer={14}>
                          CHI 2026 (1 review), TEI 2026 (3 reviews),
                          ISMAR 2025 (3 reviews, one "highly useful"), TEI 2025 (1 review, one "Special Recognitions for Outstanding Reviews")
-                         TEI 2020 (1 review),
+                         TEI 2020 (1 review), UIST 2020 (1 review)
                     </Grid.Column>
                     </Grid.Row>
                 </Grid>

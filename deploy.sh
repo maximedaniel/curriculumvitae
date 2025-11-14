e9dcd2df22fd44e7c45486e781ba868955c524e0
MaximeDanielEstiaTech <m.daniel@estia.fr>
2025-11-14 15:45:09 +0100

    improved research interests

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,20 @@ virtual_host="www.maximedaniel.me"
 user_machine="root@82.64.250.105"
 
 
+# duplicate index.qmd to printable.qmd
+cp index.qmd printable_en.qmd
+# modify printable_en.qmd to change "printable: false" to "printable: true"
+sed -i 's/printable: false/printable: true/' printable_en.qmd
+
+# duplicate index.qmd to printable.qmd
+cp printable_en.qmd printable_fr.qmd
+# modify printable_fr.qmd to change "lang: en" to "lang: fr"
+sed -i 's/lang: en/lang: fr/' printable_fr.qmd
+
+# modify printable_fr.qmd to change "en: true" to "fr: true"
+sed -i 's/en: true/fr: true/' printable_fr.qmd
+
+
 echo "Building quarto website..."
 quarto render
 
